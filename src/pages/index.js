a3aa68486f9a2a41f48b57d96a3d7e020fc65969
Scoutan <johnson.chungck@gmail.com>
2021-10-09 17:42:37 -0400

    Update index.js
    added meta tag for viewport

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
       <Head>
         <title>Responsive Layout Test</title>
         <meta name="description" content="Responsive Layout Test" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -27,13 +28,11 @@ export default function Home() {
             <PromoItemA />
             <PromoItemB />
           </div>
-          <div>
-            <PromoAbout
-              header="Our Ice Cream Philosophy"
-              textA="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-              textB="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            />
-          </div>
+          <PromoAbout
+            header="Our Ice Cream Philosophy"
+            textA="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            textB="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
         </div>
       </main>
     </div>
